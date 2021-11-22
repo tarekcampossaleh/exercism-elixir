4e57b7f209a8495c78e5df6a9b902c143a6fe35e
tarekcampossaleh <tarekcampossaleh@gmail.com>
2021-11-21 21:26:32 -0300

    feat: kitchen_calculator solution

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,20 +1,22 @@
 defmodule KitchenCalculator do
+  @cup 240.0
+  @fluid_ounce 30.0
+  @teaspoon 5.0
+  @tablespoon 15.0
+
   def get_volume({_key, value}), do: value
 
-  def to_milliliter({key, value}) when key == :cup, do: {:milliliter,value * 240}
-  def to_milliliter({key, value}) when key == :fluid_ounce, do: {:milliliter,value * 30}
-  def to_milliliter({key, value}) when key == :teaspoon, do: {:milliliter,value * 5}
-  def to_milliliter({key, value}) when key == :tablespoon, do: {:milliliter,value * 15}
-  def to_milliliter({key, value}), do: {key,value}
+  def to_milliliter({key, value}) when key == :cup, do: {:milliliter, value * @cup}
+  def to_milliliter({key, value}) when key == :fluid_ounce, do: {:milliliter, value * @fluid_ounce}
+  def to_milliliter({key, value}) when key == :teaspoon, do: {:milliliter, value * @teaspoon}
+  def to_milliliter({key, value}) when key == :tablespoon, do: {:milliliter, value * @tablespoon}
+  def to_milliliter({key, value}), do: {key, value}
 
+  def from_milliliter({_, value}, unit) when unit == :cup, do: {unit, value / @cup}
+  def from_milliliter({_, value}, unit) when unit == :teaspoon, do: {unit, value / @teaspoon}
+  def from_milliliter({_, value}, unit) when unit == :tablespoon, do: {unit, value / @tablespoon}
+  def from_milliliter({_, value}, unit) when unit == :fluid_ounce, do: {unit, value / @fluid_ounce}
+  def from_milliliter({_, value}, unit), do: {unit, value}
 
-  def from_milliliter({_key, value}, unit) when unit == :cup, do: {unit, value / 240}
-  def from_milliliter({_key, value}, unit) when unit == :fluid_ounce, do: {unit, value / 30}
-  def from_milliliter({_key, value}, unit) when unit == :teaspoon, do: {unit, value / 5}
-  def from_milliliter({_key, value}, unit) when unit == :tablespoon, do: {unit, value / 15}
-  def from_milliliter({_key, value}, unit), do: {unit, value}
-
-  def convert(volume_pair, unit) do
-    # https://exercism.org/tracks/elixir/exercises/kitchen-calculator
-  end
+  def convert(from, unit), do: to_milliliter(from) |>  from_milliliter(unit)
 end
