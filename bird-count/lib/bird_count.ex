62317652f310944dc18776355a699619ccd663f2
tarekcampossaleh <tarekcampossaleh@gmail.com>
2021-11-30 23:37:55 -0300

    wip: bird_count 8/11 tests

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -6,12 +6,11 @@ defmodule BirdCount do
   def increment_day_count([]), do: [1]
   def increment_day_count([head| tail]), do: [head+1|tail]
 
-
   def has_day_without_birds?([]), do: false
   def has_day_without_birds?([0|_tail]), do: true
   def has_day_without_birds?([_head|tail]), do: has_day_without_birds?(tail)
 
-  @spec total(any) :: nil
+  def total([]), do: 0
   def total(list) do
     # Please implement the total/1 function
   end
