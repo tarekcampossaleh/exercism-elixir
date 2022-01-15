d9663c7ca3d1e4cce2ce06f9058ceeae2ac8fd98
tarekcampossaleh <tarekcampossaleh@gmail.com>
2022-01-15 11:46:41 -0300

    wip: boutique_inventory 7/9 solutions

--- boutique-inventory/lib/boutique_inventory.ex
+++ boutique-inventory/lib/boutique_inventory.ex
@@ -1,11 +1,13 @@
 defmodule BoutiqueInventory do
-  def sort_by_price(inventory), do: inventory |> Enum.sort_by(&(&1.price))
+  def sort_by_price(inventory), do: inventory |> Enum.sort_by(& &1.price)
 
   def with_missing_price(inventory), do: inventory |> Enum.filter(&(&1.price === nil))
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
-    # Enum.map(data.quantity_by_size, fn {k,v}-> v+1 end)
+    %{
+      item
+      | quantity_by_size: Enum.into(item.quantity_by_size, %{}, fn {k, v} -> {k, v + count} end)
+    }
   end
 
   def total_quantity(item) do
