fbec905d0dcfb04b2da2aaa98e139fa560eee5af
tarekcampossaleh <tarekcampossaleh@gmail.com>
2022-03-18 12:40:30 -0300

    feat(remote_control_car): solution

--- remote-control-car/lib/remote_control_car.ex
+++ remote-control-car/lib/remote_control_car.ex
@@ -1,19 +1,30 @@
 defmodule RemoteControlCar do
-  defstruct battery_percentage: 100,
-            distance_driven_in_meters: 0,
-            nickname: "none"
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
-  def new(), do: %RemoteControlCar{}
+  def new(nickname \\ "none"), do: %RemoteControlCar{nickname: nickname}
 
-  def new(nickname), do: %RemoteControlCar{nickname: nickname}
+  def display_distance(%RemoteControlCar{} = remote_car) do
+    "#{remote_car.distance_driven_in_meters} meters"
+  end
 
-  def display_distance(remote_car), do: "#{remote_car.distance_driven_in_meters} meters"
+  def display_battery(%RemoteControlCar{} = remote_car) when remote_car.battery_percentage == 0 do
+    "Battery empty"
+  end
 
-  def display_battery(remote_car) when remote_car.battery_percentage == 0, do: "Battery empty"
+  def display_battery(%RemoteControlCar{} = remote_car) do
+    "Battery at #{remote_car.battery_percentage}%"
+  end
 
-  def display_battery(remote_car), do: "Battery at #{remote_car.battery_percentage}%"
+  def drive(%RemoteControlCar{} = remote_car) when remote_car.battery_percentage == 0 do
+    remote_car
+  end
 
-  def drive(remote_car) do
-    # Please implement the drive/1 function
+  def drive(%RemoteControlCar{} = remote_car) do
+    %{
+      remote_car
+      | battery_percentage: remote_car.battery_percentage - 1,
+        distance_driven_in_meters: remote_car.distance_driven_in_meters + 20
+    }
   end
 end
